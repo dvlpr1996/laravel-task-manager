16520e0ed4220c3d81a4ab0ba5179bdd02c548e1
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-09-24 00:13:00 +0330

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./resources/**/*.blade.php",
+	],
+	theme: {
+		screens: {
+			'xs': '360px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
+		extend: {},
+	},
+	plugins: [],
 }
