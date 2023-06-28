42d16d4abb2c9553d7a96432ae46b81fefc792bd
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-06-28 10:04:56 +0330

    implement sidebar blade file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,35 +3,7 @@
 
 <section class="relative flex min-h-screen flex-row">
 
-		<aside id="sidebar">
-				<ul class="mt-1 space-y-5">
-						<li class="aside-items">
-								<i class="fas fa-tachometer-alt mr-1"></i>
-								<a href="{{ route('dashboard.index') }}">dashboard</a>
-						</li>
-
-						<li class="aside-items">
-								<i class="fas fa-inbox mr-1"></i>
-								<a href="{{ route('inbox.index') }}">inbox</a>
-						</li>
-
-						<li class="aside-items">
-								<i class="fas fa-star mr-1"></i>
-								<a href="{{ route('today.index') }}">today</a>
-						</li>
-
-						<li class="aside-items">
-								<i class="fas fa-calendar-week mr-1"></i>
-								<a href="{{ route('tomorrow.index') }}">tomorrow</a>
-						</li>
-
-						<li class="aside-items">
-								<i class="fas fa-check-circle mr-1"></i>
-								<a href="{{ route('completed.index') }}">completed</a>
-						</li>
-						
-				</ul>
-		</aside>
+		@include('layouts.sidebar')
 
 		<main>
 				@yield('main-content')
