3d03bf703d5251d07c5640c5c0791a679cc290b9
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-06-28 10:34:29 +0330

    add alpine file

---
+++ resources/js/alpine.js
@@ -0,0 +1,38 @@
+import Alpine from 'alpinejs';
+
+document.addEventListener('alpine:init', () => {
+	Alpine.data('dropdown', () => ({
+		open: false,
+		toggle() {
+			this.open = !this.open
+		},
+		away() {
+			this.open = false
+		}
+	}));
+
+	Alpine.data('modal', () => ({
+		showModal: false,
+		toggle() {
+			this.showModal = !this.showModal
+		},
+		close() {
+			this.showModal = false
+		},
+		transition: {
+			['x-transition:enter']() {
+				return 'motion-safe:ease-out duration-300';
+			},
+			['x-transition:enter-start']() {
+				return 'opacity-0 scale-90';
+			},
+			['x-transition:enter-end']() {
+				return 'opacity-100 scale-100';
+			}
+		}
+	}));
+});
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,7 +1,19 @@
+// display msg when user is offline
 window.addEventListener("load", () => {
 	window.addEventListener("offline", () => {
-		alert("you appear to be offline. check your internet connection");
+		alert("you appear to be offline!. check your internet connection");
 	});
+});
+
+// change title when user changes tab
+let documentTitle = document.title;
+
+window.addEventListener("blur", () => {
+  document.title = "come back :-(";
+});
+
+window.addEventListener("focus", () => {
+  document.title = documentTitle;
 });
 
 // handling sidebar toggle
